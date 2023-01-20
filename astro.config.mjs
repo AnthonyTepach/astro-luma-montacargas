9e6c5ed99de7680769d15db38a20af5ad587ff19
Anthony Tepach <anthony.tepach@gmail.com>
2023-01-20 08:42:05 -0600

    footer cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://anthonytepach.github.io',
   base: '/astro-luma-montacargas',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
