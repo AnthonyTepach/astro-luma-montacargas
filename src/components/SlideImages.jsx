0ead072e7b8eb69c81e3ae53630143305ee2da40
Anthony Tepach <anthony.tepach@gmail.com>
2023-01-23 16:36:05 -0600

    slide jsx

--- src/components/SlideImages.jsx
+++ src/components/SlideImages.jsx
@@ -1,5 +1,42 @@
-function SlideImages(){
-    return (
-        <img/>
-    )
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+
+
+export default function SlideImages() {
+  
+  const [nav2, setNav2] = useState();
+
+  return (
+    <div>
+      
+     
+      <Slider
+        asNavFor={nav2}
+        ref={(slider2) => setNav2(slider2)}
+        slidesToShow={3}
+        swipeToSlide={true}
+        focusOnSelect={true}
+        onLazyLoad = {3}
+      >
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 }
