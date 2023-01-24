f0c22cf9383a3906c4e68faec00add231a0b321b
eddymtz07 <eddymtz.7@gmail.com>
2023-01-24 15:55:17 -0600

    imagenes

--- src/components/SlideImages.jsx
+++ src/components/SlideImages.jsx
@@ -16,108 +16,144 @@ export default function SlideImages() {
         onLazyLoad={'ondemand'}
         centerMode={true}
       >
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://www.mmh.com.mx/wp-content/uploads/2019/03/toyota.jpg"
-              class="h-56 w-full rounded  object-contain sm:h-72"
-            />
-
+              src="../../src/recursos/bobcat.png"
+              class="h-56 w-full rounded  object-contain sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-              <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+              <p class="mt-1 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"
-            />
-
+              src="../../src/recursos/camso.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
               <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"
-            />
-
+              src="../../src/recursos/clark.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
               <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"
-            />
-
+              src="../../src/recursos/crown.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
               <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"
-            />
-
+              src="../../src/recursos/deka.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
               <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
         <div>
           <a href="#" class="block">
             <img
               alt="Signage"
-              src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"
-            />
-
+              src="../../src/recursos/hyster.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
             <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
               <strong class="font-medium">Company Name</strong>
-
               <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
               <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
             </div>
           </a>
         </div>
+
+
+        <div>
+          <a href="#" class="block">
+            <img
+              alt="Signage"
+              src="../../src/recursos/mitsubishi.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
+
+            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+              <strong class="font-medium">Company Name</strong>
+              <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+              <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+            </div>
+          </a>
+        </div>
+
+
+        <div>
+          <a href="#" class="block">
+            <img
+              alt="Signage"
+              src="../../src/recursos/total.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
+            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+              <strong class="font-medium">Company Name</strong>
+              <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+              <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+            </div>
+          </a>
+        </div>
+
+
+        <div>
+          <a href="#" class="block">
+            <img
+              alt="Signage"
+              src="../../src/recursos/toyota.png"
+              class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-72"/>
+            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+              <strong class="font-medium">Company Name</strong>
+              <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+              <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+            </div>
+          </a>
+        </div>
+
+        
       </Slider>
     </div>
   );
